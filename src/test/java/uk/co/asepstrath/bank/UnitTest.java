820d3c1a78cef78988cef74228a1e9c192869386
Andrew Crossan <fqb22133@uni.strath.ac.uk>
2023-02-08 15:53:42 +0000

    Added User Tests to test response codes thrown in different situations

--- src/test/java/uk/co/asepstrath/bank/UnitTest.java
+++ src/test/java/uk/co/asepstrath/bank/UnitTest.java
@@ -1,10 +1,54 @@
 package uk.co.asepstrath.bank;
 
+import io.jooby.MockRouter;
+import io.jooby.StatusCode;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class UnitTest {
-    /*
-    Unit tests should be here
-    Example can be found in example/UnitTest.java
+    /**
+     * Test the welcome route.
+     * @throws Exception if something goes wrong.
+     * @since 1.0.0
      */
+    @Test
+    public void test_home_200() {
+        MockRouter router = new MockRouter(new App());
+        router.get("/", rsp -> {
+            assertEquals(StatusCode.OK, rsp.getStatusCode());
+        });
+    }
+
+    @Test
+    public void test_accounts_200() {
+        MockRouter router = new MockRouter(new App());
+        router.get("/accounts", rsp -> {
+            assertEquals(StatusCode.OK, rsp.getStatusCode());
+        });
+    }
+
+    @Test
+    public void test_accounts_404() {
+        MockRouter router = new MockRouter(new App());
+        router.get("/accounts/1", rsp -> {
+            assertEquals(StatusCode.NOT_FOUND, rsp.getStatusCode());
+        });
+    }
+
+    @Test
+    public void test_accounts_201() {
+        MockRouter router = new MockRouter(new App());
+        router.post("/accounts", rsp -> {
+            assertEquals(StatusCode.CREATED, rsp.getStatusCode());
+        });
+    }
+
+    @Test
+    public void test_account_200() {
+        MockRouter router = new MockRouter(new App());
+        router.post("/accounts/1", rsp -> {
+            assertEquals(StatusCode.OK, rsp.getStatusCode());
+        });
+    }
 }
