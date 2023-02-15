5a4e4bd2671da047576ec11656409e3ec269333a
Andrew Crossan <fqb22133@uni.strath.ac.uk>
2023-02-15 16:01:48 +0000

    Removed old test

--- src/test/java/uk/co/asepstrath/bank/IntegrationTest.java
+++ src/test/java/uk/co/asepstrath/bank/IntegrationTest.java
@@ -21,48 +21,6 @@ class IntegrationTest {
     static final OkHttpClient client = new OkHttpClient();
 
     @Test
-    public void shouldDisplayAccountInformation(int serverPort) {
-        // creates new instance of the webpage
-        Request req = new Request.Builder()
-                .url("http://localhost:" + serverPort + "/accounts")
-                .build();
-
-        try (Response rsp = client.newCall(req).execute()) {
-
-            // gets a copy of the running html code
-            assert rsp.body() != null;
-            String responseBody = rsp.body().string();
-            // defines what sections of it I actually want
-            String expectedPattern = "<div class=\"account\">\\s*<h2>([a-zA-Z]+)</h2>\\s*<p>([0-9.]+)</p>";
-
-            //regular expressions stuff - I do not really understand this but hey it works
-            //My understanding is that it takes the responseBody and finds where it is in the expectedPattern
-            Pattern pattern = Pattern.compile(expectedPattern);
-            Matcher matcher = pattern.matcher(responseBody);
-
-            // creates a array list to store the actual values we want to look at ie - returns Rachel 50 from webpage when working
-            List<String> actualValues = new ArrayList<>();
-            while (matcher.find()) {
-                actualValues.add(matcher.group(1) + "\n" + matcher.group(2));
-            }
-
-            // saving the expected values in a list to compare actualValues
-            List<String> expectedValues = Arrays.asList(
-                    "Rachel\n50.00",
-                    "Monica\n100.00",
-                    "Phoebe\n76.00",
-                    "Joey\n23.90",
-                    "Chandler\n3.00",
-                    "Ross\n54.32");
-
-            assertEquals(expectedValues, actualValues);
-            assertEquals(StatusCode.OK.value(), rsp.code());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Test
     public void test_accounts_json(int serverPort) {
         // creates new instance of the webpage
         Request req = new Request.Builder()
