ac888ae78010f2aff7d1c81b152f4097df03394e
Andrew Crossan <fqb22133@uni.strath.ac.uk>
2023-02-15 15:24:39 +0000

    Added tests for account class

--- src/test/java/uk/co/asepstrath/bank/UnitTest.java
+++ src/test/java/uk/co/asepstrath/bank/UnitTest.java
@@ -3,8 +3,11 @@ package uk.co.asepstrath.bank;
 import io.jooby.MockRouter;
 import io.jooby.StatusCode;
 import org.junit.jupiter.api.Test;
+import uk.co.asepstrath.bank.models.Account;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class UnitTest {
     /**
@@ -16,4 +19,49 @@ class UnitTest {
         MockRouter router = new MockRouter(new App());
         router.get("/", rsp -> assertEquals(StatusCode.OK, rsp.getStatusCode()));
     }
+
+    @Test
+    public void createAccount() {
+        Account a = new Account();
+        assertNotNull(a);
+    }
+
+    @Test
+    public void newAccountValue() {
+        Account a = new Account();
+        assertEquals(a.getBalance(), new BigDecimal("0.00"));
+    }
+
+    @Test
+    public void addFunds() {
+        Account a = new Account(new BigDecimal("20.00"), "Derek");
+        a.deposit(new BigDecimal("50.00"));
+        assertEquals(a.getBalance(), new BigDecimal("70.00"));
+    }
+
+    @Test
+    public void withdrawFunds() {
+        Account a = new Account(new BigDecimal("30.00"), "Derek");
+        assertThrows(Exception.class,() -> a.withdraw(new BigDecimal("100")));
+    }
+
+    @Test
+    public void superSaving() {
+        Account a = new Account(new BigDecimal("20.00"), "Derek");
+        for (int x = 0; x<=4; x++) {
+            a.deposit(new BigDecimal("10.00"));
+        }
+        for (int x = 0; x<=2; x++) {
+            a.withdraw(new BigDecimal("20.00"));
+        }
+        assertEquals(a.getBalance(), new BigDecimal("10.00"));
+    }
+
+    @Test
+    public void penniesCaring() {
+        Account a = new Account(new BigDecimal("5.45"), "Derek");
+        a.deposit(new BigDecimal("17.56"));
+        assertEquals(a.getBalance(), new BigDecimal("23.01"));
+    }
+
 }
