e2a607bab8d609266ade673dbf3b2ea43e594a41
Andrew Crossan <fqb22133@uni.strath.ac.uk>
2023-02-15 15:23:34 +0000

    Remove Tests for Account class

--- src/test/java/uk/co/asepstrath/bank/UnitTest.java
+++ src/test/java/uk/co/asepstrath/bank/UnitTest.java
@@ -19,49 +19,4 @@ class UnitTest {
         MockRouter router = new MockRouter(new App());
         router.get("/", rsp -> assertEquals(StatusCode.OK, rsp.getStatusCode()));
     }
-
-    @Test
-    public void createAccount() {
-        Account a = new Account();
-        assertNotNull(a);
-    }
-
-    @Test
-    public void newAccountValue() {
-        Account a = new Account();
-        assertEquals(a.getBalance(), new BigDecimal("0.00"));
-    }
-
-    @Test
-    public void addFunds() {
-        Account a = new Account(new BigDecimal("20.00"), "Derek");
-        a.deposit(new BigDecimal("50.00"));
-        assertEquals(a.getBalance(), new BigDecimal("70.00"));
-    }
-
-    @Test
-    public void withdrawFunds() {
-        Account a = new Account(new BigDecimal("30.00"), "Derek");
-        assertThrows(Exception.class,() -> a.withdraw(new BigDecimal("100")));
-    }
-
-    @Test
-    public void superSaving() {
-        Account a = new Account(new BigDecimal("20.00"), "Derek");
-        for (int x = 0; x<=4; x++) {
-            a.deposit(new BigDecimal("10.00"));
-        }
-        for (int x = 0; x<=2; x++) {
-            a.withdraw(new BigDecimal("20.00"));
-        }
-        assertEquals(a.getBalance(), new BigDecimal("10.00"));
-    }
-
-    @Test
-    public void penniesCaring() {
-        Account a = new Account(new BigDecimal("5.45"), "Derek");
-        a.deposit(new BigDecimal("17.56"));
-        assertEquals(a.getBalance(), new BigDecimal("23.01"));
-    }
-
 }
