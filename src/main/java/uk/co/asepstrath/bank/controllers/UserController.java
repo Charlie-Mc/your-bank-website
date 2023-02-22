5c48a90e4bde533c345caef11109d8497487dbaf
Andrew Crossan <fqb22133@uni.strath.ac.uk>
2023-02-22 18:32:24 +0000

    Removed obsolete list view, created new revised version, pagination handled and class variable accounts added for caching accounts data.

--- src/main/java/uk/co/asepstrath/bank/controllers/UserController.java
+++ src/main/java/uk/co/asepstrath/bank/controllers/UserController.java
@@ -6,8 +6,11 @@ import io.jooby.ModelAndView;
 import io.jooby.StatusCode;
 import io.jooby.annotations.*;
 import io.jooby.exception.StatusCodeException;
+import org.apache.http.annotation.Obsolete;
 import org.slf4j.Logger;
 import uk.co.asepstrath.bank.models.Account;
+import uk.co.asepstrath.bank.models.Page;
+
 import javax.sql.DataSource;
 import java.math.BigDecimal;
 import java.sql.*;
@@ -23,36 +26,75 @@ public class UserController {
     private final DataSource dataSource;
     private final Logger logger;
 
+    // Save accounts to an arraylist to prevent constant database calls
+    private ArrayList<Account> accounts = new ArrayList<>();
+
     public UserController(DataSource ds, Logger lgr) {
         dataSource = ds;
         logger = lgr;
     }
 
+    /**
+     * This is the revised list method
+     * @param format String
+     * @param ctx Context
+     * @param page Integer
+     * @return Object
+     */
+
     @GET
-    public Object accounts(@QueryParam String format, Context ctx) {
+    public Object RevisedList(@QueryParam String format, Context ctx, @QueryParam Integer page) {
         HashMap<String, Object> model = new HashMap<>();
+        ArrayList<Page> pages = new ArrayList<>();
+        // Add Page Title to the model
         model.put("title", "Accounts");
-        ArrayList<Account> accounts = new ArrayList<>();
-        try (Connection conn = dataSource.getConnection()) {
-             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users");
-             ResultSet rs = stmt.executeQuery();
-            while (rs.next()) {
-                BigDecimal temp = rs.getBigDecimal("balance");
-                Account account = new Account(rs.getString("id"),rs.getString("name"), temp,rs.getString("currency"),rs.getString("accountType"));
-                accounts.add(account);
+        if (this.accounts.isEmpty()) {
+            // If the accounts arraylist is empty, load from database
+            try (Connection conn = dataSource.getConnection()) {
+                ArrayList<Account> accounts = new ArrayList<>();
+                PreparedStatement stmt = conn.prepareStatement("SELECT * FROM users");
+                ResultSet rs = stmt.executeQuery();
+                while (rs.next()) {
+                    BigDecimal temp = rs.getBigDecimal("balance");
+                    Account account = new Account(rs.getString("id"), rs.getString("name"), temp, rs.getString("currency"), rs.getString("accountType"));
+                    accounts.add(account);
+                    this.accounts = accounts;
+                }
+                logger.info("Accounts Loaded: " + accounts.size());
+                stmt.close();
+            } catch (SQLException e) {
+                logger.error("Error connecting to database", e);
+                throw new StatusCodeException(StatusCode.SERVER_ERROR, "Error connecting to database", e);
             }
-            logger.info("Accounts Loaded: " + accounts);
-            stmt.close();
-        } catch (SQLException e) {
-            logger.error("Error connecting to database", e);
-            throw new StatusCodeException(StatusCode.SERVER_ERROR, "Error connecting to database", e);
+        }
+        // Load pagination
+        int count = 0;
+        for (Account a : accounts) {
+            count++;
+            if (count % 20 == 0) {
+                // Add page to the model
+                Page p = new Page(new ArrayList<>(this.accounts.subList(count - 20, count)), count / 20);
+                pages.add(p);
+                model.put("pages", pages);
+            }
+        }
+        if (page != null) {
+            // If the page is not null, return the accounts for that page
+            model.put("accounts", pages.get(page - 1).getAccounts());
+            pages.get(page - 1).setCurrent(true);
+            model.put("pageCount", pages.get(pages.size() - 1).count);
+            // Set the active page;
+            return new ModelAndView("account.hbs", model);
         }
         if (format != null && format.equals("json")) {
             ctx.setResponseType("application/json");
             logger.info("Accounts Loaded in JSON: " + new Gson().toJson(accounts));
             return new Gson().toJson(accounts);
         }
-        model.put("accounts", accounts);
+        // If the page is null, return the first page
+        model.put("accounts", pages.get(0).getAccounts());
+        pages.get(0).setCurrent(true);
+        model.put("pageCount", pages.get(pages.size() - 1).count);
         return new ModelAndView("account.hbs", model);
     }
 
@@ -84,6 +126,5 @@ public class UserController {
             throw new StatusCodeException(StatusCode.SERVER_ERROR, "Error connecting to database", e);
         }
         return new ModelAndView("account.hbs", model);
-
     }
 }
