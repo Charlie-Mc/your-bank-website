a900708cc9cda90d662248a394b6f99b6a3e176b
Andrew Crossan <fqb22133@uni.strath.ac.uk>
2023-03-02 23:13:58 +0000

    Add integration test for all pages 200 statys code

--- src/test/java/uk/co/asepstrath/bank/IntegrationTest.java
+++ src/test/java/uk/co/asepstrath/bank/IntegrationTest.java
@@ -1,26 +1,14 @@
 package uk.co.asepstrath.bank;
 
 import io.jooby.JoobyTest;
-import io.jooby.StatusCode;
-import kong.unirest.GenericType;
-import kong.unirest.HttpResponse;
-import kong.unirest.Unirest;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import uk.co.asepstrath.bank.controllers.TransactionController;
-import uk.co.asepstrath.bank.models.Account;
-import uk.co.asepstrath.bank.services.DatabaseService;
 
 import java.io.IOException;
-import java.sql.ResultSet;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -37,13 +25,72 @@ class IntegrationTest {
     @Test
     @DisplayName("Homepage 2OO Test")
     void testHomepage() throws IOException {
-        client = new OkHttpClient();
-
         Request request = new Request.Builder()
-                .url("http://localhost:8080/")
+                .url("http://localhost:" + 8911 + "/")
                 .build();
 
         Response response = client.newCall(request).execute();
+
+        assertEquals(200, response.code());
+    }
+
+    @Test
+    @DisplayName("Accounts 2OO Test")
+    void testAccounts() throws IOException {
+        Request request = new Request.Builder()
+                .url("http://localhost:" + 8911 + "/accounts")
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        assertEquals(200, response.code());
+    }
+
+    @Test
+    @DisplayName("Transactions 2OO Test")
+    void testTransactions() throws IOException {
+        Request request = new Request.Builder()
+                .url("http://localhost:" + 8911 + "/transactions")
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        assertEquals(200, response.code());
+    }
+
+    @Test
+    @DisplayName("Fraud 2OO Test")
+    void testTransactionsfraud() throws IOException {
+        Request request = new Request.Builder()
+                .url("http://localhost:" + 8911 + "/transactions/fraud")
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        assertEquals(200, response.code());
+    }
+
+    @Test
+    @DisplayName("Successful Transaction 2OO Test")
+    void testTransactionsSuccess() throws IOException {
+        Request request = new Request.Builder()
+                .url("http://localhost:" + 8911 + "/transactions/successful")
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        assertEquals(200, response.code());
+    }
+
+    @Test
+    @DisplayName("Successful Search 2OO Test")
+    void test_account_search() throws IOException {
+        Request request = new Request.Builder()
+                .url("http://localhost:" + 8911 + "/accounts/search?name=am")
+                .build();
+
+        Response response = client.newCall(request).execute();
+
         assertEquals(200, response.code());
     }
 }
