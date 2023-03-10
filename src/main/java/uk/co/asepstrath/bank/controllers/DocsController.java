6475d8667b12a1de3b9aca077bec66ed7c09041c
Scott Reid <scott.reid.2021@uni.strath.ac.uk>
2023-03-10 12:20:23 +0000

    Updated the location of the docs to /staff/swagger/docs from /docs also added the types of actions the api can do

--- src/main/java/uk/co/asepstrath/bank/controllers/DocsController.java
+++ src/main/java/uk/co/asepstrath/bank/controllers/DocsController.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@Path("/docs")
+@Path("/staff/swagger/docs")
 public class DocsController {
     private final DataSource ds;
     private final Logger lgr;
@@ -28,9 +28,10 @@ public class DocsController {
         HashMap<String, Object> model = new HashMap<>();
         model.put("title", "API Documentation");
         List<Route> routes = new ArrayList<>();
-        routes.add(new Route("/api/routea", "GET", "this does abc"));
-        routes.add(new Route("/api/routeb", "POST", "this does ijk"));
-        routes.add(new Route("/api/routec", "DELETE", "this does xyz"));
+        routes.add(new Route("/api/team2/accounts", "ACCOUNT", "this does abc"));
+        routes.add(new Route("/api/team2/fraud", "FRAUD", "this does ijk"));
+        routes.add(new Route("/api/team2/reversal", "REVERSAL", "this does xyz"));
+        routes.add(new Route("/api/team2/transaction", "TRANSACTION", "this does xyz"));
         model.put("routes", routes);
         return new ModelAndView("docs.hbs", model);
     }
